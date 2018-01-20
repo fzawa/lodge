1c7021dd247962ad73eafccdb81dfd9fdf224f7c
fzawa <fzawa@users.noreply.github.com>
2018-01-20 16:32:39 +0900

    [fix] "Edit Profile" ページロード時にエラーが発生するバグを修正した
    fix #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user_custom!
 
   #before_action :read_tags, except: [:sign_in, :sign_up]
   before_action :read_recent_feed, except: [:sign_in, :sign_up]
@@ -30,9 +30,10 @@ class ApplicationController < ActionController::Base
   # authentication
   #
 
-  def authenticate_user!
+  def authenticate_user_custom!
+    # do authentication only if anonymous mode is disabled
     unless current_or_guest_user then
-      super
+      authenticate_user!
     end
   end
 
